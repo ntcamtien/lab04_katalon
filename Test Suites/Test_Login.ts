492070f1e4964bf0228c4e3ce5f2eb62ff88de64
NTCamTien <phluvtien0405@gmail.com>
2024-03-26 15:26:09 +0700

    commit katalon to github

---
+++ Test Suites/Test_Login.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test_Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3ddac3ab-a041-48c5-bb52-b647d0bf17bf</testSuiteGuid>
+   <testCaseLink>
+      <guid>dc457b4a-cc18-4f60-b274-ce39a1f4581c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>80bfbf58-c631-446b-b63d-5d2571224e57</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/text_login</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>80bfbf58-c631-446b-b63d-5d2571224e57</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>803e396d-3947-4f53-ba5f-8dffcf6b6e3f</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>80bfbf58-c631-446b-b63d-5d2571224e57</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>e83b18b2-cdc4-45aa-964b-c90f6344c496</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
